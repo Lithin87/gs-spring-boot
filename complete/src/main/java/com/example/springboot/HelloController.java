f65c81ec2b397ef7460a5f4f081b8da5f03fcc32
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2021-07-04 19:37:04 +0530

    new update 3

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot and Lithin and Libin & Linda & Ritesh  !";
+		return "Greetings from Spring Boot and Lithin and Libin & Linda & Ritesh y !";
 	}
 
 }
